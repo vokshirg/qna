ef0fd4f7797934ad289fdc7aa86c8d47faef0fbc
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-03-15 13:40:19 +0600

    zeus + guard

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Question, type: :model do
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  # it { should have_many :answers }
+  it { should have_many :answers }
   it { should have_many(:answers).dependent(:destroy) }
 
 end
