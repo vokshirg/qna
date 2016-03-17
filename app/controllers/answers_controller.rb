97e3db1a5f2f20dbff3bd5d344d53bbba167fa0c
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-03-17 12:26:36 +0600

    answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,48 @@
 class AnswersController < ApplicationController
+  before_action :set_answer, only: [:show, :edit, :update, :destroy]
+  def index
+    @answers = Answer.all
+  end
+
+  def show
+  end
+
+  def new
+    @answer = Answer.new()
+  end
+
+  def edit
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @answer.update(answer_params)
+      redirect_to @answer
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @answer.destroy
+    redirect_to answers_path
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:title, :body)
+  end
+
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
 end
