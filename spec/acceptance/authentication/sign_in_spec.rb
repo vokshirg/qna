cc29c4136acbb42299232b26a28b2379cf24d813
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-03-28 07:29:23 +0600

    localization I18n

--- spec/acceptance/authentication/sign_in_spec.rb
+++ spec/acceptance/authentication/sign_in_spec.rb
@@ -10,7 +10,7 @@ feature 'User sign in', %q{
   scenario 'Registred user try to sign in' do
     sign_in(user)
 
-    expect(page).to have_content 'Signed in successfully.'
+    expect(page).to have_content I18n.t('devise.sessions.signed_in')
     expect(current_path).to eq root_path
   end
 
@@ -18,9 +18,9 @@ feature 'User sign in', %q{
     visit new_user_session_path
     fill_in 'Email', with: 'new@test.ru'
     fill_in 'Password', with: '12341234'
-    click_on 'Sign in'
+    click_on I18n.t('devise.sessions.new.sign_in')
 
-    expect(page).to have_content 'Invalid email or password.'
+    expect(page).to have_content I18n.t('devise.failure.not_found_in_database', authentication_keys: 'email')
     expect(current_path).to eq new_user_session_path
   end
 end
