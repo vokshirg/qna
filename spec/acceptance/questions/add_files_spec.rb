a9c73cd3185c4b288fc5c7b51aa84333b91b57f4
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-05-31 00:24:55 +0600

    specs & attachemnt form fixes

--- spec/acceptance/questions/add_files_spec.rb
+++ spec/acceptance/questions/add_files_spec.rb
@@ -13,13 +13,17 @@ feature 'Add files to question', %q{
     visit new_question_path
   end
 
-  scenario 'User add file when create question' do
+  scenario 'User add any files when create question', js: true do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'Test text'
     attach_file 'File', "#{Rails.root}/spec/acceptance_helper.rb"
+    page.find(".add_fields").click
+    within all('.nested-fields').last do
+      attach_file 'File' , "#{Rails.root}/spec/rails_helper.rb"
+    end
     click_on I18n.t("helpers.submit.create", model: I18n.t('activerecord.models.question'))
 
     expect(page).to have_link 'acceptance_helper.rb', href: '/uploads/attachment/file/1/acceptance_helper.rb'
-
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
   end
 end
